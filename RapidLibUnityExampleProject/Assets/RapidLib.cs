1163c3de0e4b2254783f5a069cfb8cda23fc0616
marco <marcogillies@gmail.com>
2016-10-31 15:31:18 +0000

    ported the bare bones of the API

--- RapidLibUnityExampleProject/Assets/RapidLib.cs
+++ RapidLibUnityExampleProject/Assets/RapidLib.cs
@@ -4,23 +4,66 @@ using System;
 using System.Runtime.InteropServices;
 
 public class RapidLib: MonoBehaviour {
-	//Lets make our calls from the Plugin
-	[DllImport ("RapidLibPlugin")]
-	private static extern int PrintANumber();
-	
-	[DllImport ("RapidLibPlugin")]
-	private static extern IntPtr PrintHello();
-	
-	[DllImport ("RapidLibPlugin")]
-	private static extern int AddTwoIntegers(int i1,int i2);
+    //Lets make our calls from the Plugin
 
-	[DllImport ("RapidLibPlugin")]
-	private static extern float AddTwoFloats(float f1,float f2);	
-	
-	void Start () {
-		Debug.Log(PrintANumber());
-		Debug.Log(Marshal.PtrToStringAuto (PrintHello()));
-		Debug.Log(AddTwoIntegers(2,2));
-		Debug.Log(AddTwoFloats(2.5F,4F));
-	}
+    [DllImport("RapidLibPlugin")]
+    private static extern IntPtr createRegressionModel();
+
+    [DllImport("RapidLibPlugin")]
+    private static extern int getNumInputs(IntPtr model);
+
+    [DllImport("RapidLibPlugin")]
+    private static extern IntPtr createTrainingSet();
+
+    [DllImport("RapidLibPlugin")]
+    private static extern void addTrainingExample(IntPtr trainingSet, double[] inputs, int numInputs, double[] outputs, int numOutputs);
+
+    [DllImport("RapidLibPlugin")]
+    private static extern int getNumTrainingExamples(IntPtr trainingSet);
+
+    [DllImport("RapidLibPlugin")]
+    private static extern double getInput(IntPtr trainingSet, int i, int j);
+
+    [DllImport("RapidLibPlugin")]
+    private static extern double getOutput(IntPtr trainingSet, int i, int j);
+
+    [DllImport("RapidLibPlugin")]
+    private static extern bool train(IntPtr model, IntPtr trainingSet);
+
+    //   [DllImport ("RapidLibPlugin")]
+    //private static extern int PrintANumber();
+
+    //[DllImport ("RapidLibPlugin")]
+    //private static extern IntPtr PrintHello();
+
+    //[DllImport ("RapidLibPlugin")]
+    //private static extern int AddTwoIntegers(int i1,int i2);
+
+    //[DllImport ("RapidLibPlugin")]
+    //private static extern float AddTwoFloats(float f1,float f2);	
+
+    void Start () {
+		//Debug.Log(PrintANumber());
+		//Debug.Log(Marshal.PtrToStringAuto (PrintHello()));
+		//Debug.Log(AddTwoIntegers(2,2));
+		//Debug.Log(AddTwoFloats(2.5F,4F));
+        IntPtr model = createRegressionModel();
+        Debug.Log(model);
+        Debug.Log(getNumInputs(model));
+        Debug.Log("new version");
+
+        IntPtr trainingSet = createTrainingSet();
+        double[] inputs = { 1.0, 2.0 };
+        double[] outputs = { 1.0, 2.0, 3.0 };
+        addTrainingExample(trainingSet, inputs, inputs.Length, outputs, outputs.Length);
+        //addTrainingExample(trainingSet);
+        //addTrainingExample(trainingSet);
+        Debug.Log(getNumTrainingExamples(trainingSet));
+        Debug.Log(getInput(trainingSet, 0, 1));
+        Debug.Log(getOutput(trainingSet, 0, 0));
+
+        Debug.Log(train(model, trainingSet));
+        Debug.Log(getNumInputs(model));
+
+    }
 }
--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -8,27 +8,89 @@
 // ------------------------------------------------------------------------
 // Plugin itself
 
+#include <vector>
+#include <memory>
+#include "regression.h"
+
+
+std::vector < std::unique_ptr <regression> > regression_models;
+std::vector < std::unique_ptr <std::vector<trainingExample>> > training_sets;
 
 // Link following functions C-style (required for plugins)
 extern "C"
 {
 
+EXPORT_API regression * createRegressionModel() {
+	regression_models.push_back(std::unique_ptr<regression>(new regression(3, 4)));
+	return regression_models.back().get();
+}
+
+EXPORT_API int getNumInputs(regression *model) {
+	return  model->getNumInputs();
+}
+
+
+EXPORT_API std::vector<trainingExample > * createTrainingSet() {
+	training_sets.push_back(std::unique_ptr<std::vector<trainingExample >>(new std::vector<trainingExample >()));
+	return training_sets.back().get();
+}
+
+EXPORT_API void addTrainingExample(std::vector<trainingExample> *trainingSet, double *inputs, int numInputs, double *outputs, int numOutputs) {
+	trainingExample tempExample;
+	//tempExample.input = { 0.2, 0.7 };
+	for (int i = 0; i < numInputs; i++) {
+		tempExample.input.push_back(inputs[i]);
+	}
+	for (int i = 0; i < numOutputs; i++) {
+		tempExample.output.push_back(outputs[i]);
+	}
+	//tempExample.input.insert(tempExample.input.begin(), inputs, inputs+numInputs);
+	//tempExample.output = { 3.0 };
+	//tempExample.output.insert(tempExample.input.begin(), outputs, outputs + numOutputs);
+	trainingSet->push_back(tempExample);
+}
+
+EXPORT_API int getNumTrainingExamples(std::vector<trainingExample> *trainingSet) {
+	return trainingSet->size();
+}
+
+
+EXPORT_API double getInput(std::vector<trainingExample> *trainingSet, int i, int j) {
+	if (i < trainingSet->size() && j < (*trainingSet)[i].input.size()) {
+		return (*trainingSet)[i].input[j];
+	}
+	return 0.0;
+}
+
+EXPORT_API double getOutput(std::vector<trainingExample> *trainingSet, int i, int j) {
+	if (i < trainingSet->size() && j < (*trainingSet)[i].output.size()) {
+		return (*trainingSet)[i].output[j];
+	}
+	return 0.0;
+}
+
+EXPORT_API bool train(regression *model, std::vector<trainingExample> *trainingSet) {
+	model->initialize();
+	return  model->train(*trainingSet);
+}
+
+
 // The functions we will call from Unity.
 //
-const EXPORT_API char*  PrintHello(){
-	return "Hello";
-}
-
-int EXPORT_API PrintANumber(){
-	return 5;
-}
-
-int EXPORT_API AddTwoIntegers(int a, int b) {
-	return a + b;
-}
-
-float EXPORT_API AddTwoFloats(float a, float b) {
-	return a + b;
-}
+//const EXPORT_API char*  PrintHello(){
+//	return "Hello";
+//}
+//
+//int EXPORT_API PrintANumber(){
+//	return 5;
+//}
+//
+//int EXPORT_API AddTwoIntegers(int a, int b) {
+//	return a + b;
+//}
+//
+//float EXPORT_API AddTwoFloats(float a, float b) {
+//	return a + b;
+//}
 
 } // end of export C block
